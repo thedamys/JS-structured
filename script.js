fa225de8df6bc35ecbad17e1ed58d132d3edc485
thedamys <dev.team.app1@gmail.com>
2017-03-05 00:15:48 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+/*Starting*/
+function init(){
+}
+
+function createHTMLElement(eleHTML){
+  return document.createElement();
+}
+
+fuction createText(text){
+  return document.createTextNode(text);
+}
+
